d2051fe147e247cff90d8b7b6d8ec64373984582
Pk <pkp2729732@gmail.com>
2026-02-26 21:53:50 +0530

    operator

--- operator/operator.js
+++ operator/operator.js
@@ -54,16 +54,17 @@
 
 
 // marksheet in ternory operator
-let marks = prompt("Enter your marks:");
+//let marks = prompt("Enter your marks:");
 // const maxMarks = 100;
 // const minPassMarks = 30;
 // let result = (marks >= 0 && marks <= maxMarks) ?
 //     (marks >= minPassMarks40 ? "Pass" : "Fail") :
 //     "Invalid marks entered.";
+//console.log("Result:", result);
 
-
-
-
-
-
-console.log("Result:", result);
+let marks = Number (prompt("Enter your marks:"));
+let result = (marks >= 30 && marks <= 50) ? "grade d" :
+(marks >= 50 && marks <=  70) ? "grade c" :
+(marks >= 70 && marks <= 90) ? "grade b" :
+(marks >= 90 && marks <= 100) ? "grade a" : "fail"
+console.log("The student has", result);
