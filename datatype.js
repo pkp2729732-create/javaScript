0f6cccaaae122330d8d853886c2b007fec556339
Pk <pkp2729732@gmail.com>
2026-03-20 14:15:27 +0530

    About js

--- datatype.js
+++ datatype.js
@@ -1,8 +1,10 @@
-// two types of datatype in js
-// premitve                           non premitve or refrensh
-// 1- string                           object
-// 2- number                           arrey
-// 3- boolean
-// 4- undefined
-// 5- null
-// 6- symble
+/*two types of datatype in js
+premitve                           non premitve or refrensh
+1- string                           object
+2- number                           arrey
+3- boolean
+4- undefined
+5- null
+6- symble
+these are immutable
+*/
--- first.js
+++ first.js
@@ -1,7 +1,15 @@
-// javascript is a light weight interpreted JIT compiled scripting language used for front end development.
+// javascript is a light weight, Multiparadigm, event driven, functional, imperative, procedural, interpreted JIT compiled, Object oriented programming language.used for front end development.
+// js is a scripting language.
+//js is a interpreter based lang.
+//js is a wealy typed lang.
+// js is a run time based lang.
+//js is a blocking nature environment lang.
+//js is a synchronous programing lang but it behaves like a asynchronous as well.(example- set interval, set timeout, promise)
+// note: js is a single threaded programming language.
+// js is a backword compatible programing language. 
 // keywords--> some reserved words that has special meaning and perform specific tasks ex- if else while switch etc
 
-
+//To display output on console -->
 
 console.log("hello world");
 console.log('world hello');
